599ed394883539cc913c7c547fc64ef31097e71d
reefabegum <reefa.begum@cloudreach.com>
2022-08-16 11:47:29 +0530

    security_gropups

---
+++ securitygroups.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "elk_server" {
+  name        = "elk-server"
+  description = "Allow elk server inbound traffic"
+  vpc_id      = aws_vpc.elk_vpc.id
+
+  ingress {
+    description      = "Allow port 80"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "Allow port 22"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
